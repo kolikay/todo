33236125035724448be3ae94371f0b975ac9b7eb
kolikay <kolikay1989@gmail.com>
2021-10-26 19:32:36 +0100

    worked on home page design

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -9,14 +9,56 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: const EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0 ),
-        child: Column(
-          children: [
-           const Icon(Icons.list),
-           const Text('Todoey'),
-          ],
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 40.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const Text(
+                  '12 todos',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 300.0,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0))),
+            ),
+          ),
+        ],
       ),
     );
   }
