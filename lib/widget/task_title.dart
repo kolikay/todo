256cac57ab8849ccf5abb50afaead535f5ec34c3
kolikay <kolikay1989@gmail.com>
2021-10-27 20:55:08 +0100

    worked on task title

--- lib/widget/task_title.dart
+++ lib/widget/task_title.dart
@@ -1,18 +1,34 @@
-
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
-
-class  TaskTitle extends StatelessWidget {
+class TaskTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-     title: const Text('this is a task'),
-    //  trailing: Checkbox(
-    //    value: false,
-    //    onChanged: , 
-    //  ),
-        );
+      title: Text('this is a task'),
+      trailing: TaskCheckBox(),
+    );
+  }
+}
+
+class TaskCheckBox extends StatefulWidget {
+  @override
+  _TaskCheckBoxState createState() => _TaskCheckBoxState();
+}
+
+class _TaskCheckBoxState extends State<TaskCheckBox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(()  {
+         isChecked = newValue!;
+        });
+      },
+    );
   }
 }
